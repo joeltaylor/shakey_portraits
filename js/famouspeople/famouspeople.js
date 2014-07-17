243215dfdf34c6ebd1715dcf8f5c38b010e4c742
Joel Taylor <joeltaylor424@gmail.com>
2014-07-17 18:12:12 -0400

    Cleanup and fixes

---
+++ js/famouspeople/famouspeople.js
@@ -0,0 +1,193 @@
+(function($) {
+
+  $.fn.famousPeople = function( options ) {
+
+    // Establish our default settings
+    var settings = $.extend({
+      number         :1,
+      width        : null,
+      height       : null,
+      source       : null,
+      quotes       : null,
+      links        : null
+    }, options);
+
+    // Get basics about sprite image
+    var width = settings.width,
+        height = settings.height,
+        totalWidth = width * settings.number,
+        startingPoints = [],
+        firstQuote = [],
+        quoteCycle = null,
+        i = 0;
+
+    // Now find the sprite starting points
+    for (i = 0; i < totalWidth; i += width){
+      startingPoints.push(i);
+    }
+
+    // Create famous people objects to store quotes and info
+    var people = [];
+    var famousPeople = {};
+    for (i = 1; i <= startingPoints.length; i++){
+        famousPeople['person' + i] = {
+        location : startingPoints[i-1],
+        quotes   : settings.quotes[i-1],
+        links    : settings.links[i-1],
+        id       : i
+      };
+      people.push(famousPeople['person' + i]);
+    }
+    // Shuffle the people around
+    people.sort(function() { return 0.5 - Math.random() });
+
+    // Quote cycle
+      quoteCycle = function(person){
+        firstQuote = person.quotes.shift();
+        person.quotes.push(firstQuote);
+      };
+
+    // Interactions
+    var id,
+        active = false,
+        frame,
+        animationCount,
+        rotation,
+        rotationBack,
+        rotateHome;
+
+    rotation = function () {
+        animationCount += 1;
+        frame.rotate({
+          animateTo:5,
+          duration: 100,
+          center: ["65%","50%"],
+          callback: function () { if (animationCount < 4){rotationBack()}}
+        });
+    };
+
+    rotationBack = function () {
+        animationCount += 1;
+        frame.rotate({
+          animateTo: -5,
+          duration: 100,
+          center: ["65%","50%"],
+          callback: function () { if (animationCount < 4){rotation()} else {return rotateHome()}}
+        });
+    };
+
+    rotateHome = function () {
+        frame.stopRotate();
+        frame.rotate({
+          angle:0,
+          duration: 100,
+          center: ["65%","50%"]
+        });
+       active = false;
+    };
+
+    // Attach mouseover event
+    this.on('mouseover', 'img', function() {
+      if (active){
+        return;
+      } else {
+        active = true;
+        frame = $(this).closest('div');
+        // Switch the quotes
+        id = $(this).data("info");
+        $person = famousPeople["person" + id];
+        $(this).parent('a').siblings('.quoteholder').find('span').fadeOut(1).text($person.quotes[0]).fadeIn(500);
+        quoteCycle($person);
+
+        // Call rotation functions
+        animationCount = 0;
+        rotation();
+      }
+    });
+
+
+    return this.each( function(element) {
+      var $person,
+          image,
+          quote,
+          wrapper,
+          link,
+          quoteholder;
+
+
+      // Place the images and give initial quote
+      for (var i = 0; i < settings.show; i++) {
+
+        $person = people.pop();
+        link = $('<a>',{
+          href: $person.links,
+          "class" : "fp_link"
+        })
+
+        image = $('<img>',{
+          src: settings.source,
+          attr: {
+            "data-info": $person.id
+          },
+          css: {
+            position: "absolute",
+            left: -$person.location,
+            "min-width": totalWidth
+          }
+        });
+
+
+        quote = $('<span>', {
+          "class" : "quote",
+          text : $person.quotes[0]
+        });
+
+        // Because firefox is being fickle
+        quoteholder = $('<div>',{
+          "class" : "quoteholder",
+          css: {
+            display: "table",
+            height: "inherit"
+          }
+        });
+
+        // Cycle the quote
+        quoteCycle($person);
+
+
+        wrapper = $('<div>', {
+          "class" : "famous-person",
+          css:{
+            width : width,
+            height : height,
+            overflow : "hidden",
+            position : "relative"
+          }
+        }).append(image).append(quote);
+
+        image.wrap(link);
+        quote.wrap(quoteholder);
+
+        $(this).append(wrapper);
+      };
+    });
+  };
+
+})(jQuery);
+$(document).ready(function(){
+  jQuery("#sidebar").famousPeople({
+    number : 5,
+    show   : 3,
+    height : 451,
+    width  : 455,
+    source : "js/famouspeople/famous_people_sprite.png",
+    quotes : [
+                ["The Importance of Using Shmoop","This Picture is Boring and Gray.","I'm a Wilde and crazy kid."],
+                ["The coldest winter I ever spent was a summer without Shmoop.", "I'll be your Huckleberry.", "Who you callin' Sam Clemens?" ],
+                ["Beat! Beat! Shmoop!", "I may write free verse, but you still need to pay for my book.", "Better with a beard."],
+                ["I'm the old man and the typewriter.", "I'm glad I'm not an amputee. I'd hate to have to say farewell to my arms.", "Brevity is the soul of wit. Or so I'm told."],
+                ["In each of my books is a Colonel (Brandon) of truth.","Popular fiction by and about women—what a novel idea!","Hey ladies: you're welcome for creating Mr. Dar"]
+              ],
+    links  : ["http://www.shmoop.com/oscar-wilde/", "http://www.shmoop.com/mark-twain/", "http://www.shmoop.com/walt-whitman/", "http://www.shmoop.com/ernest-hemingway/", "http://www.shmoop.com/jane-austen/"]
+  });
+});
